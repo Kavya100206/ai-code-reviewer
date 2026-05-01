cc994aec8c152f3f1ee3a9aab36accdf336e1135
Kavya Raghavendran <kavyaraghavendran10@gmail.com>
2026-05-02 02:12:13 +0530

    test: add bad code for AI review

--- src/test.js
+++ src/test.js
@@ -1,2 +1,8 @@
-const password = "123456";
-console.log(password);
+const password = "hardcoded123";
+const query = "SELECT * FROM users WHERE id = " + userId;
+
+function doSomething(a, b, c, d, e, f) {
+  var x = 1;
+  eval(userInput);
+  console.log(password);
+}
